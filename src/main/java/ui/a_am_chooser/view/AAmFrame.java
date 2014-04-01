66777066e73190c185dce8475c84c076d4a5df2a
bakeraw2 <notarget1>
2014-04-01 21:09:24 +0200

    Poprawki

--- src/main/java/ui/a_am_chooser/view/AAmFrame.java
+++ src/main/java/ui/a_am_chooser/view/AAmFrame.java
@@ -2,29 +2,58 @@ package ui.a_am_chooser.view;
 
 import database.dao.TextsDao;
 import ui.a_am_chooser.logic.AAmController;
+import util.ApplicationUtils;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
 
 /**
  * Author: Daniel
  */
 public class AAmFrame extends JDialog {
 
-    public static final int WIDTH = 250;
-    public static final int HEIGHT = 150;
+    public static final int HEIGHT = 600;
+    public static final int WIDTH = 960;
 
     public AAmFrame(JDialog nextScreen) {
         setUpDialog();
         initializeComponents(nextScreen);
-        setLocationRelativeTo(null);
     }
 
     private void setUpDialog() {
-        setSize(new Dimension(WIDTH, HEIGHT));
+        setSize(WIDTH, HEIGHT);
         setTitle(TextsDao.getText("view.CategoryChooser.title"));
         setModal(true);
         setResizable(false);
+        setIgnoreRepaint(false);
+        setUndecorated(true);
+        ApplicationUtils.setApplicationIcon(this);
+        setLocationRelativeTo(null);
+        setWindowRemoveble();
+    }
+
+    private void setWindowRemoveble() {
+        final Point point = new Point();
+        addMouseListener(new MouseAdapter() {
+            public void mousePressed(MouseEvent e) {
+                if (!e.isMetaDown()) {
+                    point.x = e.getX();
+                    point.y = e.getY();
+                }
+            }
+        });
+        addMouseMotionListener(new MouseMotionAdapter() {
+            public void mouseDragged(MouseEvent e) {
+                if (!e.isMetaDown()) {
+                    Point p = getLocation();
+                    setLocation(p.x + e.getX() - point.x,
+                            p.y + e.getY() - point.y);
+                }
+            }
+        });
     }
 
     private void initializeComponents(JDialog nextScreen) {
@@ -34,7 +63,8 @@ public class AAmFrame extends JDialog {
         add(aAmPanel);
 
         controller.setaCategoryBtn(aAmPanel.getaCategoryBtn());
-        controller.setaAmCategoryBtn(aAmPanel.getaAmCategoryBtn());
+        controller.setaAmCategoryBtn(aAmPanel.getAmCategoryBtn());
+        controller.setCloseBtn(aAmPanel.getCloseBtn());
 
         addWindowListener(controller.getWindowListener());
     }
--- src/main/java/ui/a_am_chooser/view/AAmPanel.java
+++ src/main/java/ui/a_am_chooser/view/AAmPanel.java
@@ -1,13 +1,19 @@
 package ui.a_am_chooser.view;
 
+import util.Const;
+import util.ImageUtils;
+
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * Author: Daniel
  */
 public class AAmPanel extends JPanel {
-    private JButton aCategoryBtn;
-    private JButton aAmCategoryBtn;
+    private JLabel aCategoryBtn;
+    private JLabel AmCategoryBtn;
+    private JLabel chooseCategoryLbl;
+    private JLabel closeBtn;
 
     public AAmPanel() {
         setUpPanel();
@@ -15,31 +21,64 @@ public class AAmPanel extends JPanel {
     }
 
     private void setUpPanel() {
+        setBackground(Const.Colors.BACKGROUND_COLOR);
         setLayout(null);
     }
 
     private void initializeComponents() {
-        JLabel info = new JLabel("Wybierz kategorię");
-        info.setBounds(10, 10, 200, 30);
-        aCategoryBtn = createBtn("A", 10, 60, 100, 40);
-        aAmCategoryBtn= createBtn("AM", 120, 60, 100, 40);
+        aCategoryBtn = createBtn("a.png", 50, 440);
+        AmCategoryBtn = createBtn("am.png", 650, 440);
+        chooseCategoryLbl = getChooseCategoryLbl();
+        closeBtn = createCloseBtn();
 
-        add(info);
         add(aCategoryBtn);
-        add(aAmCategoryBtn);
+        add(AmCategoryBtn);
+        add(chooseCategoryLbl);
+        add(closeBtn);
     }
 
-    private JButton createBtn(String btnLabel, int x, int y, int width, int height) {
-        JButton jButton = new JButton(btnLabel);
-        jButton.setBounds(x, y, width, height);
-        return jButton;
+    private JLabel createCloseBtn() {
+        final ImageIcon imgUn = ImageUtils.getProgramImage("close_btn.png");
+        final JLabel button = new JLabel(imgUn);
+        button.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        button.setBounds(915, 5, 40, 40);
+        return button;
     }
 
-    public JButton getaCategoryBtn() {
+    private JLabel getChooseCategoryLbl() {
+        final ImageIcon imgUn = ImageUtils.getProgramImage("wybierz.png");
+
+        final JLabel button = new JLabel(imgUn);
+        button.setBounds(350, 440, 280, 85);
+        return button;
+    }
+
+    private JLabel createBtn(String imgUnPath, int x, int y) {
+        final ImageIcon imgUn = ImageUtils.getProgramImage(imgUnPath);
+
+        final JLabel button = new JLabel(imgUn);
+        button.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        button.setBounds(x, y, 280, 85);
+        return button;
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        ImageIcon programImage = ImageUtils.getProgramImage("bg.png");
+        g.drawImage(programImage.getImage(), 0, 0, null);
+    }
+
+    public JLabel getaCategoryBtn() {
         return aCategoryBtn;
     }
 
-    public JButton getaAmCategoryBtn() {
-        return aAmCategoryBtn;
+    public JLabel getAmCategoryBtn() {
+        return AmCategoryBtn;
+    }
+
+    public JLabel getCloseBtn() {
+        return closeBtn;
     }
 }
